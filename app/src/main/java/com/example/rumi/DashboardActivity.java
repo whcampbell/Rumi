38db3a06c12470d3cab88f50997744afb3013750
Will <Campbell>
2021-10-29 13:37:27 -0500

    Added bottom nav to xml  - icon files added  - switched dashboardActivity to relative layout

--- app/src/main/java/com/example/rumi/DashboardActivity.java
+++ app/src/main/java/com/example/rumi/DashboardActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setSelectedItemId(R.id.dashboard);
     }
+
 }
