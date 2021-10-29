e9f95513011ea044357663fd13d004f74129cad0
Dustin <dustint.esser@gmail.com>
2021-10-28 21:27:26 -0500

    Main menu functional. Menu items at the bottom do not display correctly.

---
+++ app/src/main/java/com/example/rumi/agreements.java
@@ -0,0 +1,24 @@
+package com.example.rumi;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class agreements extends Fragment {
+
+    public agreements() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_agreements, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/rumi/chores.java
@@ -0,0 +1,24 @@
+package com.example.rumi;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class chores extends Fragment {
+
+    public chores() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chores, container, false);
+    }
+}
