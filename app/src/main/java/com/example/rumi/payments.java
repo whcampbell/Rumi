13679b9cdf70e896410a82b8c3aa0cbe723db641
Dustin <dustint.esser@gmail.com>
2021-11-11 00:09:05 -0600

    added "+" Button for Payments fragment (creates new textView each time it is pressed)

--- app/src/main/java/com/example/rumi/payments.java
+++ app/src/main/java/com/example/rumi/payments.java
@@ -4,12 +4,22 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 
 public class payments extends Fragment {
+
+
+    private Button addPaymentButton;
 
 
     public payments() {
@@ -20,6 +30,27 @@ public class payments extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_payments, container, false);
+        View view = inflater.inflate(R.layout.fragment_payments, container, false);
+        addPaymentButton = view.findViewById(R.id.addPaymentButton);
+
+        addPaymentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView temp = new TextView(view.getContext());
+                temp.setGravity(Gravity.CENTER_HORIZONTAL);
+                temp.setText("this is a payments temp notification");
+                LinearLayout layout = getView().findViewById(R.id.myLinearView);
+
+
+                temp.setGravity(Gravity.LEFT);
+
+                layout.addView(temp);
+            }
+        });
+
+        return view;
+
+
+
     }
 }
