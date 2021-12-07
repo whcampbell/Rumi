d228c1b75ef2937ce00e7e64a5043ce1fd761bca
Mollie Christensen <mschristens4@wisc.edu>
2021-12-07 16:58:13 -0600

    added new agreement/addendum activities

---
+++ app/src/main/java/com/example/rumi/newAddendumActivity.java
@@ -0,0 +1,32 @@
+package com.example.rumi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class newAddendumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_agreement);
+
+    }
+
+    public void onClickCreate(View view) {
+        String body = ((EditText)findViewById(R.id.editTextBody)).getText().toString();
+        String signer = ((EditText)findViewById(R.id.editTextSigner)).getText().toString();
+
+        // TODO: add to database
+
+        finish();
+    }
+
+    public void onClickCancel(View view) {
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/rumi/newAgreementActivity.java
@@ -0,0 +1,31 @@
+package com.example.rumi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class newAgreementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_agreement);
+
+    }
+
+    public void onClickCreate(View view) {
+        String title = ((EditText)findViewById(R.id.editTextTitle)).getText().toString();
+
+        // TODO: add to database
+
+        finish();
+    }
+
+    public void onClickCancel(View view) {
+        finish();
+    }
+}
