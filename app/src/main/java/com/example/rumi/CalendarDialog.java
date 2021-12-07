ca4e8f47b48c97fdd2443427cf670e16a4b4da7e
Will <Campbell>
2021-12-06 21:20:48 -0600

    More work on calendar  - some data management for new event creation  - date selected dialog now has listview for events  - new event activity now has cancel button

--- app/src/main/java/com/example/rumi/CalendarDialog.java
+++ app/src/main/java/com/example/rumi/CalendarDialog.java
@@ -5,8 +5,14 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatDialogFragment;
+
+import java.util.ArrayList;
 
 public class CalendarDialog extends AppCompatDialogFragment {
     private String date;
@@ -17,9 +23,27 @@ public class CalendarDialog extends AppCompatDialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        ArrayList<String> testEvents = new ArrayList<>();
+        String e1 = "First Event\n5:30, Edward Klief";
+        String e2 = "Second Event\n2:00, Mosse Humanities";
+        String e3 = "Third Event\n8:00, CS 1240";
+        testEvents.add(e1);
+        testEvents.add(e2);
+        testEvents.add(e3);
+
+
+        LayoutInflater inflater = getLayoutInflater();
+        View jankView = inflater.inflate(R.layout.events_dialog, null);
+        ListView lv = jankView.findViewById(R.id.list);
+
+        ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, testEvents);
+
+        lv.setAdapter(adapter);
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(date)
-                .setMessage("This is an event")
+                .setView(jankView)
                 .setPositiveButton("New Event", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
--- app/src/main/java/com/example/rumi/newEventActivity.java
+++ app/src/main/java/com/example/rumi/newEventActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class newEventActivity extends AppCompatActivity {
     boolean pm = true;
@@ -17,7 +19,41 @@ public class newEventActivity extends AppCompatActivity {
 
     }
 
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_pm:
+                if (checked)
+                    pm = true;
+                    break;
+            case R.id.radio_am:
+                if (checked)
+                    pm = false;
+                    break;
+        }
+    }
+
     public void onClickCreate(View view) {
+        String name = ((EditText)findViewById(R.id.eTextName)).getText().toString();
+        String location = ((EditText)findViewById(R.id.eTextLocation)).getText().toString();
+        String date = ((EditText)findViewById(R.id.editTextDate)).getText().toString();
+        String time = ((EditText)findViewById(R.id.editTextTime)).getText().toString();
+        if (pm) {
+            time = time + "pm";
+        } else {
+            time = time + "am";
+        }
+
+        // TODO add to database
+        //  may involve parsing date/time for format
+
+        finish();
+    }
+
+    public void onClickCancel(View view) {
         finish();
     }
 }
