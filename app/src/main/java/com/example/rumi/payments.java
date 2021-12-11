4d8fcdd4814dd312fda22e0d8510c05ed84f6b32
Dustin <dustint.esser@gmail.com>
2021-12-10 19:53:22 -0600

    minor tweaks to payments to auto refresh

--- app/src/main/java/com/example/rumi/payments.java
+++ app/src/main/java/com/example/rumi/payments.java
@@ -4,12 +4,12 @@ package com.example.rumi;
 import static android.app.Activity.RESULT_OK;
 import static android.content.ContentValues.TAG;
 
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 
 import android.os.Parcelable;
@@ -91,10 +91,10 @@ public class payments extends Fragment {
                             unpaidList.setAdapter(adapterString);
                             unpaidList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
                                 @Override
-                                public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                                     Map<String, Object> payment = new HashMap<String, Object>();
                                     payment.put("paid", "true");
-                                    ArrayList<QueryDocumentSnapshot> list =  (ArrayList<QueryDocumentSnapshot>) unpaidList.getTag();
+                                    ArrayList<QueryDocumentSnapshot> list = (ArrayList<QueryDocumentSnapshot>) unpaidList.getTag();
                                     QueryDocumentSnapshot document = list.get(position);
                                     String docId = document.getId();
                                     db.collection("payment").document(docId).update(payment);
@@ -168,6 +168,7 @@ public class payments extends Fragment {
             //add any values you want into the Map
 
             db.collection("payment").add(payment);
+            refresh();
         }
 
     }
@@ -188,6 +189,86 @@ public class payments extends Fragment {
         }
     }
     public void refresh(){
+
+        TextView middle = view.findViewById(R.id.middle);
+        addPaymentButton = view.findViewById(R.id.addPaymentButton);
+        unpaidList = view.findViewById(R.id.unpaidList);
+        paidList = view.findViewById(R.id.paidList);
+
+
+
+
+
+
+        db.collection("payment")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            ArrayList<String> unpaid = new ArrayList<String>();
+                            ArrayList<String> paid = new ArrayList<String>();
+                            ArrayList<QueryDocumentSnapshot> unpaidId = new ArrayList<QueryDocumentSnapshot>();
+                            ArrayList<QueryDocumentSnapshot> paidId = new ArrayList<QueryDocumentSnapshot>();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.e(TAG, document.getId() + " => " + document.getData());
+                                if(document != null && document.getData().get("paid").equals("false")){
+                                    unpaid.add(String.format("Owe amount: %s", document.getData().get("amount")));
+                                    unpaidId.add(document);
+                                }else{
+                                    paid.add(String.format("Paid amount: %s", document.getData().get("amount")));
+                                    paidId.add(document);
+                                }
+                            }
+                            ArrayAdapter adapterString = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, unpaid);
+                            unpaidList.setTag(unpaidId);
+                            unpaidList.setAdapter(adapterString);
+                            unpaidList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                    Map<String, Object> payment = new HashMap<String, Object>();
+                                    payment.put("paid", "true");
+                                    ArrayList<QueryDocumentSnapshot> list = (ArrayList<QueryDocumentSnapshot>) unpaidList.getTag();
+                                    QueryDocumentSnapshot document = list.get(position);
+                                    String docId = document.getId();
+                                    db.collection("payment").document(docId).update(payment);
+                                    refresh();
+                                }
+                            });
+                            adapterString = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, paid);
+                            paidList.setTag(paidId);
+                            paidList.setAdapter(adapterString);
+                            paidList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                                    Map<String, Object> payment = new HashMap<String, Object>();
+                                    payment.put("paid", "false");
+
+                                    ArrayList<QueryDocumentSnapshot> list =  (ArrayList<QueryDocumentSnapshot>) paidList.getTag();
+                                    QueryDocumentSnapshot document = list.get(position);
+                                    String docId = document.getId();
+                                    db.collection("payment").document(docId).update(payment);
+                                    refresh();
+                                }
+                            });
+                        } else {
+                            Log.e(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
+        addPaymentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getActivity(), newPaymentActivity.class);
+                startActivityForResult(intent, RequestCode);
+
+
+            }
+        });
+
+
 
     }
 }
