e9618b415bd092a39d5164b95a8c66601be1690a
Will <Campbell>
2021-12-13 01:05:46 -0600

    Dashboard full functional  - pulls conversations from db  - can post new conversations, adds to db  - shows first comment under header  - can add comments in separate activity  - db pushing and pulling  - displayed in order of time added (probably?)

--- app/src/main/java/com/example/rumi/ConvoActivity.java
+++ app/src/main/java/com/example/rumi/ConvoActivity.java
@@ -1,24 +1,112 @@
 package com.example.rumi;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class ConvoActivity extends AppCompatActivity {
+    FirebaseFirestore db;
+    DocumentReference dr;
+    ArrayList<String> comms;
+    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_convo);
+
+        db = FirebaseFirestore.getInstance();
+
         String head = getIntent().getStringExtra("head");
         TextView text = findViewById(R.id.textHead);
         text.setText(head);
+
+        comms = new ArrayList<>();
+        ListView commList = findViewById(R.id.commentList);
+        adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, comms);
+        commList.setAdapter(adapter);
+
+        dr = db.collection("Houses").document(MainActivity.houseNumber)
+                .collection("Conversations").document(head);
+
+        dr.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot doc = task.getResult();
+                    if (doc.exists()) {
+                        Map<String, Object> map = doc.getData();
+                        for(Map.Entry entry : map.entrySet()) {
+                            comms.add(0, (String)entry.getValue());
+                        }
+                        adapter.notifyDataSetChanged();
+                    } else {
+                        Log.e("err", "error finding document");
+                    }
+                }
+            }
+        });
+
+
     }
 
     public void onClickCancel(View view) {
         finish();
+    }
+
+    public void onClickAdd(View view) {
+
+        EditText newCommText = findViewById(R.id.editTextNewComment);
+        String newComm = newCommText.getText().toString();
+
+        if (newComm.equals("")) {
+            return;
+        }
+
+        newCommText.setText("");
+
+        dr.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot doc = task.getResult();
+                    if (doc.exists()) {
+                        Map<String, Object> map = doc.getData();
+                        int index = map.size();
+                        map.put("comment" + (index + 1), newComm);
+                        dr.update(map);
+                        comms.add(newComm);
+                        adapter.notifyDataSetChanged();
+                    } else {
+                        HashMap<String, Object> map = new HashMap<>();
+                        map.put("comment1", newComm);
+                        dr.set(map);
+                        comms.add(newComm);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/rumi/dashboard.java
+++ app/src/main/java/com/example/rumi/dashboard.java
@@ -2,9 +2,11 @@ package com.example.rumi;
 
 import static android.content.ContentValues.TAG;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,15 +16,29 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class dashboard extends Fragment {
     private ArrayList<DashConvo> convoList;
     private RecyclerView reView;
     private RecyclerAdapter.RecyclerClickListenter listener;
+    private RecyclerAdapter adapter;
+    private FirebaseFirestore db;
 
 
     public dashboard() {
@@ -32,36 +48,102 @@ public class dashboard extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
         TextView houseTextView = view.findViewById(R.id.houseNumber);
 
+        EditText enterText = view.findViewById(R.id.editTextPostHead);
+        Button postButton = view.findViewById(R.id.button6);
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String enterHead = enterText.getText().toString();
+                Context context = getContext();
+                String message = "Enter text to post a message";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, message, duration);
 
+                if (enterHead.equals("")) {
+                    toast.show();
+                } else {
+                    addToDatabase(enterHead);
+                    convoList.add(0, new DashConvo(enterHead, "No comments yet"));
+                    adapter.notifyDataSetChanged();
+                    enterText.setText("");
+                }
+            }
+        });
 
         houseTextView.setText(MainActivity.getHouseName());
 
         reView = view.findViewById(R.id.Recycler);
-
-
-
         convoList = new ArrayList<>();
-        convoList.add(new DashConvo("once upon a midnight dreary while I pondered weak and weary\n" +
-                "over many a quaint and curious volume of forgotten lore\n" +
-                "while I nodded nearly napping suddenly there came a tapping", "this is a comment"));
-        convoList.add(new DashConvo("head2", "this is another"));
-        convoList.add(new DashConvo("head3", "and this is a third"));
-        convoList.add(new DashConvo("how bout we add another head that's on the longer end of things", "and then the comment won't be too long"));
-        convoList.add(new DashConvo("head5", "commememembklt"));
-        convoList.add(new DashConvo("heeed6", "the fitnessgram pacer test is a multistage aerobic fitness examination"));
+
+        db = FirebaseFirestore.getInstance();
+        CollectionReference cr = db.collection("Houses").document(MainActivity.houseNumber)
+                .collection("Conversations");
+
+        cr.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    convoList.clear();
+                    for (QueryDocumentSnapshot doc : task.getResult()) {
+                        String hString = doc.getId();
+                        String cString = doc.getString("comment1");
+                        if (cString == null || cString.equals("")) {
+                            cString = "No comments yet";
+                        }
+                        convoList.add(0, new DashConvo(hString, cString));
+                    }
+                }
+            }
+        });
+
+        if (convoList.size() == 0) {
+            convoList.add(new DashConvo("Pretty quiet in here...", "Add a conversation?"));
+        }
 
         setAdapter();
 
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        CollectionReference cr = db.collection("Houses").document(MainActivity.houseNumber)
+                .collection("Conversations");
+
+        cr.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    convoList.clear();
+                    for (QueryDocumentSnapshot doc : task.getResult()) {
+                        String hString = doc.getId();
+                        String cString = doc.getString("comment1");
+                        if (cString == null || cString.equals("")) {
+                            cString = "No comments yet";
+                        }
+                        convoList.add(0, new DashConvo(hString, cString));
+                    }
+                    if (convoList.size() == 0) {
+                        convoList.add(new DashConvo("Pretty quiet in here...", "Add a conversation?"));
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+    }
+
     private void setAdapter() {
         setOnClickListener();
-        RecyclerAdapter adapter = new RecyclerAdapter(convoList, listener);
+        adapter = new RecyclerAdapter(convoList, listener);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
         reView.setLayoutManager(layoutManager);
         reView.setItemAnimator(new DefaultItemAnimator());
@@ -79,8 +161,11 @@ public class dashboard extends Fragment {
         };
     }
 
-    public void onClickPost(View view) {
-        // TODO add new convo to database and top of recycler view
+    private void addToDatabase(String header) {
+        DocumentReference dr = db.collection("Houses").document(MainActivity.houseNumber)
+                .collection("Conversations").document(header);
+        HashMap<String, Object> map = new HashMap<>();
+        dr.set(map);
     }
 
 }
